678ddaae0b1ab54e54753331e38b87aa43dd80e1
LuCg13 <luis.gomezm1203@gmail.com>
2024-03-15 23:29:46 -0600

    Cambios en page.test y page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,5 @@ export const metadata = {
 };
 
 export default function Page() {
-  return <h1>Esto es una prueba de primer parcial</h1>;
+  return <h1>Esto es una prueba para el primer parcial</h1>;
 }
